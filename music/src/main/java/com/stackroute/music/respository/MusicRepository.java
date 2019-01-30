079c39412b66441be214ada2c16f9ec78cff8d09
amit-s-k <amitsk2014@gmail.com>
2019-01-30 20:11:04 +0530

    modified docker compose file

--- music/src/main/java/com/stackroute/music/respository/MusicRepository.java
+++ music/src/main/java/com/stackroute/music/respository/MusicRepository.java
@@ -4,6 +4,5 @@ import com.stackroute.music.model.Music;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MusicRepository extends MongoRepository<Music, Integer> {
 }
